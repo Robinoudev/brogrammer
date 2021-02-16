aca47acebb3df1f6566c99a769880b583cdec317
Robin Oude Vrielink <robin@oudevrielink.xyz>
2021-02-16 15:20:57 +0100

    Added mu4e support in emacs

--- config/emacs/files/.doom/config.el
+++ config/emacs/files/.doom/config.el
@@ -22,29 +22,34 @@
 ;; (setq doom-font (font-spec :family "Inconsolata" :size 12 :weight 'medium)
 ;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
 
-(setq doom-font (font-spec :family "JetBrainsMono Nerd Font" :size 16 :weight 'Light)
+(setq doom-font (font-spec :family "FiraCode Nerd Font" :size 18 :weight 'Regular)
       doom-variable-pitch-font (font-spec :family "Ubuntu" :size 15)
-      doom-big-font (font-spec :family "JetBrainsMono Nerd Font" :size 24))
+      doom-big-font (font-spec :family "FiraCode Nerd Font" :size 24))
 (after! doom-themes
   (setq doom-themes-enable-bold t
         doom-themes-enable-italic t))
 (custom-set-faces!
-  '(font-lock-comment-face :slant Italic :weight Light)
-  '(font-lock-keyword-face :slant Italic)
-  '(font-lock-type-face :weight Regular))
+  ;; '(font-lock-comment-face :slant Italic :weight Regular)
+  '(font-lock-comment-face :font "JetBrainsMono Nerd Font" :slant Italic)
+  '(font-lock-keyword-face :font "JetBrainsMono Nerd Font" :slant Italic)
+  '(font-lock-type-face :font "JetBrainsMono Nerd Font" :weight Regular))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-miramare)
+(setq doom-theme 'doom-nord-light)
 
+;; ORG
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/personal/org/")
+(setq org-ellipsis " ▾")
 
 (after! org
   (require 'org-bullets)
-  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
+  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
+  (add-hook 'org-mode-hook 'auto-fill-mode)
+  (add-hook 'org-mode-hook 'writeroom-mode))
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
@@ -80,5 +85,76 @@
 
 (setq doom-modeline-vcs-max-length 20)
 
-;; set auth file
-(setq auth-sources '("~/.authinfo"))
+;; EMAIL
+(add-to-list 'load-path "/usr/local/share/emacs/site-lisp/mu4e")
+;;(require 'smtpmail)
+(setq user-mail-address "robin@oudevrielink.net"
+      user-full-name  "Robin Oude Vrielink"
+      ;; I have my mbsyncrc in a different folder on my system, to keep it separate from the
+      ;; mbsyncrc available publicly in my dotfiles. You MUST edit the following line.
+      ;; Be sure that the following command is: "mbsync -c ~/.config/mu4e/mbsyncrc -a"
+      ;; mu4e-get-mail-command "mbsync -c ~/.config/mu4e/mbsyncrc -a"
+      mu4e-update-interval  300
+      mu4e-main-buffer-hide-personal-addresses t
+      message-send-mail-function 'smtpmail-send-it
+      starttls-use-gnutls t
+      smtpmail-starttls-credentials '(("smtp.fastmail.com" 587 nil nil))
+      mu4e-sent-folder "/personal/Sent"
+      mu4e-drafts-folder "/personal/Drafts"
+      mu4e-trash-folder "/personal/Trash"
+      mu4e-maildir-shortcuts
+      '(("/personal/Inbox"      . ?i)
+        ("/personal/Sent Items" . ?s)
+        ("/personal/Drafts"     . ?d)
+        ("/personal/Trash"      . ?t)))
+
+(defvar my-mu4e-account-alist
+  '(("personal"
+     (mu4e-sent-folder "/personal/Sent")
+     (mu4e-drafts-folder "/personal/Drafts")
+     (mu4e-trash-folder "/personal/Trash")
+     (mu4e-compose-signature
+       (concat
+         "Robin Oude Vrielink\n"
+         "robin@oudevrielink.net\n"))
+     (user-mail-address "robin@oudevrielink.net")
+     (smtpmail-default-smtp-server "smtp.fastmail.com")
+     (smtpmail-smtp-server "smtp.fastmail.com")
+     (smtpmail-smtp-user "robin@oudevrielink.net")
+     (smtpmail-stream-type starttls)
+     (smtpmail-smtp-service 587))
+    ("work"
+     (mu4e-sent-folder "/work/Sent")
+     (mu4e-drafts-folder "/work/Drafts")
+     (mu4e-trash-folder "/work/Trash")
+     (mu4e-compose-signature
+       (concat
+         "Robin Oude Vrielink\n"
+         "r.oudevrielink@nerds.company\n"))
+     (user-mail-address "r.oudevrielink@nerds.company")
+     (smtpmail-default-smtp-server "smtp.gmail.com")
+     (smtpmail-smtp-server "smtp.gmail.com")
+     (smtpmail-smtp-user "r.oudevrielink@nerds.company")
+     (smtpmail-stream-type starttls)
+     (smtpmail-smtp-service 587))))
+
+(defun my-mu4e-set-account ()
+  "Set the account for composing a message."
+  (let* ((account
+          (if mu4e-compose-parent-message
+              (let ((maildir (mu4e-message-field mu4e-compose-parent-message :maildir)))
+                (string-match "/\\(.*?\\)/" maildir)
+                (match-string 1 maildir))
+            (completing-read (format "Compose with account: (%s) "
+                                     (mapconcat #'(lambda (var) (car var))
+                                                my-mu4e-account-alist "/"))
+                             (mapcar #'(lambda (var) (car var)) my-mu4e-account-alist)
+                             nil t nil nil (caar my-mu4e-account-alist))))
+         (account-vars (cdr (assoc account my-mu4e-account-alist))))
+    (if account-vars
+        (mapc #'(lambda (var)
+                  (set (car var) (cadr var)))
+              account-vars)
+      (error "No email account found"))))
+
+(add-hook 'mu4e-compose-pre-hook 'my-mu4e-set-account)
--- config/emacs/files/.doom/init.el
+++ config/emacs/files/.doom/init.el
@@ -35,7 +35,7 @@
        hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
        ;;hydra
        indent-guides     ; highlighted indent columns
-       ;;ligatures         ; ligatures and symbols to make your code pretty again
+       ligatures         ; ligatures and symbols to make your code pretty again
        ;;minimap           ; show a map of the code on the side
        modeline          ; snazzy, Atom-inspired modeline, plus API
        ;;nav-flash         ; blink cursor line after big motions
@@ -44,12 +44,12 @@
        (popup +defaults)   ; tame sudden yet inevitable temporary windows
        ;;tabs              ; a tab bar for Emacs
        treemacs          ; a project drawer, like neotree but cooler
-       ;;unicode           ; extended unicode support for various languages
+       unicode           ; extended unicode support for various languages
        vc-gutter         ; vcs diff in the fringe
        vi-tilde-fringe   ; fringe tildes to mark beyond EOB
        ;;window-select     ; visually switch windows
        workspaces        ; tab emulation, persistence & separate workspaces
-       ;;zen               ; distraction-free coding or writing
+       zen               ; distraction-free coding or writing
 
        :editor
        (evil +everywhere); come to the dark side, we have cookies
@@ -58,7 +58,7 @@
        ;;(format +onsave)  ; automated prettiness
        ;;god               ; run Emacs commands without modifier keys
        ;;lispy             ; vim for lisp, for people who don't like vim
-       ;;multiple-cursors  ; editing in many places at once
+       multiple-cursors  ; editing in many places at once
        ;;objed             ; text object editing for the innocent
        ;;parinfer          ; turn lisp into python, sort of
        ;;rotate-text       ; cycle region at point between text candidates
@@ -68,7 +68,7 @@
        :emacs
        dired             ; making dired pretty [functional]
        electric          ; smarter, keyword-based electric-indent
-       ;;ibuffer         ; interactive buffer management
+       ibuffer         ; interactive buffer management
        undo              ; persistent, smarter undo for your inevitable mistakes
        vc                ; version-control and Emacs, sitting in a tree
 
